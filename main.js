0659b49f8f882772651aa89d599ef3ca8ed674bc
Alef <60857146+alefz0@users.noreply.github.com>
2023-12-23 19:43:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,84 @@
+
+
+
+
+
+
+
+//--------------- exercise 1 ---------------
+
+alert("Hello world")
+
+//--------------- exercise 2 ---------------
+
+let firstNumber = 10
+let secondNumber = 5
+let result = firstNumber + secondNumber
+alert(result)
+
+//--------------- exercise 3 ---------------
+
+let value = "10"
+if(typeof value === 'number') {
+   alert(`${value} é um número`)
+} else {
+   alert(`${value} não é um número`)
+}
+
+//--------------- exercise 4 ---------------
+
+let value = "10"
+if(typeof value === 'string') {
+   alert(`${value} é uma string`)
+} else {
+   alert(`${value} não é uma string`)
+}
+
+//--------------- exercise 5 ---------------
+
+let value = true
+if(typeof value === 'boolean') {
+   alert(`${value} é um boolean`)
+} else {
+   alert(`${value} não é um boolean`)
+}
+
+//--------------- exercise 6 ---------------
+
+let firstNumber = 10
+let secondNumber = 5
+let result = firstNumber - secondNumber
+alert(result)
+
+//--------------- exercise 7 ---------------
+
+let firstNumber = 10
+let secondNumber = 5
+let result = firstNumber * secondNumber
+alert(result)
+
+//--------------- exercise 8 ---------------
+
+let firstNumber = 10
+let secondNumber = 5
+let result = firstNumber / secondNumber
+alert(result)
+
+//--------------- exercise 9 ---------------
+
+let value = 25
+if(value % 2 === 0) {
+    alert(`${value} é um número par`)
+} else {
+    alert(`${value} não é um n[umero par]`)
+}
+
+//--------------- exercise 10 ---------------
+
+let value = 25
+if(value % 2 === 0) {
+    alert(`${value} não é um número ímpar`)
+} else {
+    alert(`${value} é um número ímpar`)
+}
+
